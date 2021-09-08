06a89f0f77f52e1e5d1c0f1eb3bf8a5c8bc651a8
Vedant Madane <6527493+VedantMadane@users.noreply.github.com>
2021-09-08 19:08:57 +0530

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 // import Dashboard from '../views/Dashboard.vue'
 import Projects from '../views/Projects.vue'
-import Team from '../views/Team.vue'
 
 Vue.use(VueRouter)
 
@@ -83,11 +82,6 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../components/Homepage.vue')
-  },
-  {
-    path: '/team',
-    name: 'team',
-    component: Team
   },
   {
     path: '/update/:key',
